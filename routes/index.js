5b7bd0679060cc5f9f6cfe1c946d7ab5206a813a
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-10-31 09:36:12 +0900

    page design

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,11 @@ const controller = require("../controller/Cmain");
 
 router.get("/", controller.get_Index);
 
+router.get("/feed", controller.get_Feed);
+
+router.get("/calender", controller.get_Calender);
+
+router.get("/timer", controller.get_Timer);
+
+router.get("/myPage", controller.get_MyPage);
 module.exports = router;
