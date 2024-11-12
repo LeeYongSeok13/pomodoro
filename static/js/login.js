853b4e0b727da89bb34ccabc9fcb12d0adbc4b2a
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-11-12 10:12:38 +0900

    로그인관련 페이지 완료(CSS 포함)

--- static/js/login.js
+++ static/js/login.js
@@ -31,7 +31,6 @@ async function loginUser() {
 
     if (response.status === 200 && data.success) {
       // 로그인 성공 시 페이지 리디렉션
-      console.log("1234");
       window.location.href = "/"; // 로그인 성공 후 홈으로 이동
     } else {
       // 로그인 실패 시 modal 띄우기
